b85b111df4f0fcfe87da40be07b17e2ba028a738
Y2K <yeyintkyaw300@gmail.com>
2021-11-26 22:07:23 -0800

    plzzz

--- routes/web.php
+++ routes/web.php
@@ -33,13 +33,13 @@ Route::get('/articles', [ArticleController::class, 'index']);
 Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
 Route::get('/articles/delete/{id}', [ArticleController::class,'delete']);
 Route::get('/articles/add', [ArticleController::class,'add']);
-Route::post('/articles/add', [ArticleController::class,'create'])->name('articles.create');
+Route::post('/articles/create', [ArticleController::class,'create'])->name('articles.create');
 Route::get('/articles/edit/{id}', [ArticleController::class,'edit']);
 Route::post('/articles/edit/{id}', [ArticleController::class,'update']);
 Route::post('/comments/add', [CommentController::class,'create']);
 Route::get('/comments/delete/{id}', [CommentController::class,'delete']);
 Route::get('/categories/add', [CategoryController::class,'add']);
-Route::post('/categories/add', [CategoryController::class,'create'])->name('categories.create');
+Route::post('/categories/create', [CategoryController::class,'create'])->name('categories.create');
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\ArticleController::class, 'home'])->name('home');
